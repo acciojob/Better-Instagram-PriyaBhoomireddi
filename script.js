34292f4dc89472a88d4694f96fb9852bf40af39e
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-08-02 22:09:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,3 +23,18 @@ window.onload = function() {
     });
   });
 };
+it('should drag and drop images', () => {
+    const draggable = Cypress.$('#div1')[0]; 
+    const droppable = Cypress.$('#div5')[0]; 
+
+    const coords = droppable.getBoundingClientRect();
+
+    draggable.dispatchEvent(new MouseEvent('mousedown', { clientX: 0, clientY: 0 }));
+    draggable.dispatchEvent(new MouseEvent('mousemove', { clientX: 10, clientY: 0 }));
+    draggable.dispatchEvent(new MouseEvent('mousemove', { clientX: coords.x + 10, clientY: coords.y + 10 }));
+    draggable.dispatchEvent(new MouseEvent('mouseup'));
+
+    cy.get('#div5').within(() => {
+      cy.get('.image').should('have.length', 1); 
+    });
+});
